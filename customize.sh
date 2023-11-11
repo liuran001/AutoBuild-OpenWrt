21041b1b3bc57f1644efae7a777e8e5e8f9d029b
--global <--global>
2023-11-12 04:06:59 +0800

    push

--- customize.sh
+++ customize.sh
@@ -1,15 +1,59 @@
 #!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
-#=================================================
-#1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
-#2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "s#hostname='ImmortalWrt'#hostname='BakaWrt'#g" openwrt/package/base-files/files/bin/config_generate
+sed -i "s#timezone='UTC'#timezone='Asia/Shanghai'#g" openwrt/package/base-files/files/bin/config_generate
+sed -i "s#system.ntp.server='time1.apple.com'#system.ntp.server='ntp.aliyun.com'#g" openwrt/package/base-files/files/bin/config_generate
+sed -i "s#system.ntp.server='time1.google.com'#system.ntp.server='time1.cloud.tencent.com'#g" openwrt/package/base-files/files/bin/config_generate
+sed -i "s#system.ntp.server='time.cloudflare.com'#system.ntp.server='time.ustc.edu.cn'#g" openwrt/package/base-files/files/bin/config_generate
 
-#3. Replace with JerryKuKu’s Argon
-#rm openwrt/package/lean/luci-theme-argon -rf
+cp -f files/banner openwrt/package/base-files/files/etc/
+cp files/distfeeds.conf.1 openwrt/package/system/opkg/files/
+cp -f files/20_migrate-feeds openwrt/package/system/opkg/files/
+
+echo '127.0.0.1 bakawrt bakawrt.local' >> openwrt/package/base-files/files/etc/hosts
+echo '::1     bakawrt bakawrt.local' >> openwrt/package/base-files/files/etc/hosts
+
+cd openwrt/package
+rm -rf feeds/luci/luci-theme-argon
+rm -rf feeds/luci/luci-app-argon-config
+
+cd custom-packages
+git clone https://github.com/jerrykuku/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config
+git clone https://github.com/sirpdboy/netspeedtest
+git clone https://github.com/Erope/openwrt_nezha
+git clone https://github.com/linkease/istore
+git clone https://github.com/linkease/nas-packages-luci
+git clone https://github.com/linkease/nas-packages
+git clone https://github.com/kenzok8/openwrt-packages kenzok8
+git clone https://github.com/sirpdboy/luci-app-poweroffdevice
+rm -rf kenzok8/luci-app-argon-config
+rm -rf kenzok8/luci-theme-argon
+
+cd ../..
+
+mkdir -p turboacc_tmp ./package/turboacc
+cd turboacc_tmp 
+git clone https://github.com/chenmozhijin/turboacc -b package
+cd ../package/turboacc
+git clone https://github.com/fullcone-nat-nftables/nft-fullcone
+git clone https://github.com/chenmozhijin/turboacc
+mv ./turboacc/luci-app-turboacc ./luci-app-turboacc
+rm -rf ./turboacc
+cd ../..
+cp -f turboacc_tmp/turboacc/hack-6.1/952-add-net-conntrack-events-support-multiple-registrant.patch ./target/linux/generic/hack-6.1/952-add-net-conntrack-events-support-multiple-registrant.patch
+cp -f turboacc_tmp/turboacc/hack-6.1/953-net-patch-linux-kernel-to-support-shortcut-fe.patch ./target/linux/generic/hack-6.1/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
+cp -f turboacc_tmp/turboacc/pending-6.1/613-netfilter_optional_tcp_window_check.patch ./target/linux/generic/pending-6.1/613-netfilter_optional_tcp_window_check.patch
+rm -rf ./package/libs/libnftnl ./package/network/config/firewall4 ./package/network/utils/nftables
+mkdir -p ./package/network/config/firewall4 ./package/libs/libnftnl ./package/network/utils/nftables
+cp -r ./turboacc_tmp/turboacc/shortcut-fe ./package/turboacc
+cp -RT ./turboacc_tmp/turboacc/firewall4-$(grep -o 'FIREWALL4_VERSION=.*' ./turboacc_tmp/turboacc/version | cut -d '=' -f 2)/firewall4 ./package/network/config/firewall4
+cp -RT ./turboacc_tmp/turboacc/libnftnl-$(grep -o 'LIBNFTNL_VERSION=.*' ./turboacc_tmp/turboacc/version | cut -d '=' -f 2)/libnftnl ./package/libs/libnftnl
+cp -RT ./turboacc_tmp/turboacc/nftables-$(grep -o 'NFTABLES_VERSION=.*' ./turboacc_tmp/turboacc/version | cut -d '=' -f 2)/nftables ./package/network/utils/nftables
+rm -rf turboacc_tmp
+echo "# CONFIG_NF_CONNTRACK_CHAIN_EVENTS is not set" >> target/linux/generic/config-6.1
+echo "# CONFIG_SHORTCUT_FE is not set" >> target/linux/generic/config-6.1
+
+
+./scripts/feeds update -a
+./scripts/feeds install -a
